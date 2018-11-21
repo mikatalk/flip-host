f539b3c97040a08de31485df0bad9ce5a013c8c4
Mika I <mikatalk@users.noreply.github.com>
2018-11-21 08:22:44 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const pkg = require(__dirname + '/package.json')
+const prompts = require('prompts')
+const websites = pkg.websites
+
+async function start() {
+  
+  const hostFile = fs.readFileSync('/etc/hosts', 'utf8')
+  const head = hostFile.match(/(.*)(?:# BEGIN hosts added by flip-host)/s)
+  const body = hostFile.match(/(?:# BEGIN hosts added by flip-host)(.*)(?:# END hosts added by flip-host)/s)
+  const tail = hostFile.match(/(?:# END hosts added by flip-host)(.*)/s)
+
+  if (body) {
+    console.log(
+      '- - - - - - - - - - - - - - - - - - - - - -\n' +
+      '-               MANAGED HOSTS             -\n' +
+      '- - - - - - - - - - - - - - - - - - - - - -' + 
+      body[1] + 
+      '- - - - - - - - - - - - - - - - - - - - - -'
+    )
+    // console.log(body[1])
+  } else {
+    console.log('Oh, hello first time user!')
+  }
+
+  let response = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Pick a website',
+    choices: websites.map(el => ({title: el, value: el})),
+    initial: Math.max(0, Math.min(websites.length-1, (pkg.settings['last-selected'] || 0)))
+  })
+  const website = response.value
+
+  // save last choice
+  pkg.settings['last-selected'] = Math.max(0, Math.min(websites.length-1, websites.indexOf(website)))
+  fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
+
+  response = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Pick a environment',
+    choices: pkg.ips,
+    initial: 0
+  })
+  const server = response.value
+
+  if (!website || !server) {
+    console.log('Not this time... Bye bye...')
+    return
+  }
+  
+  console.log(`Set host for '${website}' to '${server}'`) 
+
+  fs.writeFileSync('/etc/hosts', (head ? head[1] : '') +
+  '# BEGIN hosts added by flip-host' +
+  '\n' + (body ? body[1] : '').split('\n').filter(val => val.indexOf(website) === -1 && val !== '').join('\n') +
+  '\n' + server + '\t' + website + '\n' + 
+  '# END hosts added by flip-host\n' +
+  (tail ? tail[1] : ''))
+}
+
+start()
